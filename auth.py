18b88f5915313037527352d306eec742573c2628
techtricker <81182453+techtricker@users.noreply.github.com>
2025-04-30 12:03:22 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.hash import bcrypt
+
+# Secret key and algorithm
+SECRET_KEY = "Parr@matta"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_password(plain_password, hashed_password):
+    return bcrypt.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return bcrypt.hash(password)
