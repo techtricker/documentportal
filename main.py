f84585f9efb703bcdb8471f006b7d41df587794d
techtricker <81182453+techtricker@users.noreply.github.com>
2025-04-30 22:12:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],    # or ["*"] for all origins (use only for testing)
+    allow_origins=origins,    # or ["*"] for all origins (use only for testing)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
