cd145ff3352f42c36bdd86c18f8feccad452e5b4
techtricker <81182453+techtricker@users.noreply.github.com>
2025-04-30 11:13:13 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 20 14:29:58 2025
-
-@author: AshokKumarSathasivam
-"""
-
-from sqlalchemy import Column, Integer, String, DateTime
-from database import Base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, LargeBinary, DateTime
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from datetime import datetime
 
+DATABASE_URL = "mysql+pymysql://username:password@localhost/documentportal"  # Change this to your MySQL credentials
 
+# Create SQLAlchemy engine and session
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class FileMeta(Base):
-    __tablename__ = 'filemeta'
-    id = Column(Integer, primary_key=True, index=True)
-    panel = Column(String(100))
-    path = Column(String(255))
-    filename = Column(String(255), unique=True, index=True)
-    filetype = Column(String(100))
-    created_by = Column(String(100))
-    created_date = Column(DateTime, default=datetime.now())
-    
+# Base class for models
+Base = declarative_base()
+
+# PanelMaster Model
 class PanelMaster(Base):
-    __tablename__ = 'panel_master'
-    
-    id = Column(Integer, primary_key=True)
-    panel_name = Column(String(100), unique=True)
-    description = Column(String(255))
-    
+    __tablename__ = "panel_master"
+    panel_id = Column(Integer, primary_key=True, index=True)
+    file_meta = relationship("FileMeta", back_populates="panel")
+
+# FileMeta Model
+class FileMeta(Base):
+    __tablename__ = "file_meta"
+    file_meta_id = Column(Integer, primary_key=True, index=True)
+    panel_id = Column(Integer, ForeignKey("panel_master.panel_id"))
+    file_name = Column(String)
+    file_data = Column(LargeBinary)
+
+    # Relationship to PanelMaster
+    panel = relationship("PanelMaster", back_populates="file_meta")
+
+# User Model
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, index=True)
+
+# UserAssignment Model
+class UserAssignment(Base):
+    __tablename__ = "user_assignment"
+    user_assignment_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    panel_id = Column(Integer, ForeignKey("panel_master.panel_id"))
+    secret_code = Column(String)
+    qr_code = Column(LargeBinary)
+
+# UserScanLog Model
+class UserScanLog(Base):
+    __tablename__ = "user_scan_log"
+    log_id = Column(Integer, primary_key=True, index=True)
+    user_assignment_id = Column(Integer, ForeignKey("user_assignment.user_assignment_id"))
+    scan_datetime = Column(DateTime, default=datetime.utcnow)
+
+# Create tables in the database
+Base.metadata.create_all(bind=engine)
